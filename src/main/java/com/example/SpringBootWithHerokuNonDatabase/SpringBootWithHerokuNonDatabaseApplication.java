5aab029b75b770d72a5a68f8b2527e4880d21b31
HajPhan <hajphan97@gmail.com>
2019-08-17 00:34:02 +0700

    Upload new project :smile:

---
+++ src/main/java/com/example/SpringBootWithHerokuNonDatabase/SpringBootWithHerokuNonDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithHerokuNonDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithHerokuNonDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithHerokuNonDatabaseApplication.class, args);
+	}
+
+}
